343dd2e22349363d407753f92aa0703d7b2277f6
Renxing Liu <liurenxing1993@gmail.com>
2017-10-18 16:19:22 +0800

    Add functionality to delete multiple person

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
@@ -22,10 +23,10 @@ public class DeleteCommand extends UndoableCommand {
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
-    private final Index targetIndex;
+    private ArrayList<Index> targetIdxs;
 
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public DeleteCommand(ArrayList<Index> targetIndex) {
+        this.targetIdxs = targetIndex;
     }
 
 
@@ -34,6 +35,49 @@ public class DeleteCommand extends UndoableCommand {
 
         List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
 
+        ArrayList<Integer> executableIdx = new ArrayList<>();
+        ArrayList<Integer> unExeIdx = new ArrayList<>();
+        Boolean hasExecutableIdx = false;
+
+        for (Index idx : targetIdxs) {
+            int intIdx = idx.getZeroBased();
+            if (intIdx < lastShownList.size()) {
+                executableIdx.add(intIdx);
+                hasExecutableIdx = true;
+            } else {
+                unExeIdx.add(intIdx);
+            }
+        }
+
+        if (!hasExecutableIdx) {throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);}
+
+
+
+        for (int idx : executableIdx) {
+            try {
+                model.deletePerson(lastShownList.get(idx));
+            } catch (PersonNotFoundException e) {
+                assert false : "The target person cannot be missing";
+            }
+        }
+    /*
+        ArrayList<ReadOnlyPerson> peopleToDelete = new ArrayList<>();
+        for (int idx : executableIdx) {
+            peopleToDelete.add(lastShownList.get(idx));
+        }
+
+        for (ReadOnlyPerson person : peopleToDelete) {
+            try {
+                model.deletePerson(person);
+            } catch (PersonNotFoundException e) {
+                assert false : "The target person cannot be missing";
+            }
+        }
+        */
+
+        return new CommandResult("Delete Command WIP");
+
+    /*
         if (targetIndex.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
@@ -47,12 +91,13 @@ public class DeleteCommand extends UndoableCommand {
         }
 
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+        */
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DeleteCommand // instanceof handles nulls
-                && this.targetIndex.equals(((DeleteCommand) other).targetIndex)); // state check
+                && this.targetIdxs.equals(((DeleteCommand) other).targetIdxs)); // state check
     }
 }
--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,6 +2,10 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.DeleteCommand;
@@ -12,12 +16,51 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
+    private int STI(String a) {
+        a = a.trim();
+        return Integer.parseInt(a);
+    }
+
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns an DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+        args = args.trim();
+        List<String> indexStrs = Arrays.asList(args.split(" "));
+        ArrayList<Index> indexes = new ArrayList<>();
+
+        for (int i=0; i<indexStrs.size(); i++) {
+            indexes.add( Index.fromOneBased(STI(indexStrs.get(i))-i));
+        }
+
+        /*
+        for (String indexStr : indexStrs) {
+            try {
+                indexes.add(ParserUtil.parseIndex(indexStr));
+            } catch (IllegalValueException e) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+            }
+        }
+        */
+        /*
+        ArrayList<Index> indexFinal = new ArrayList<>();
+
+        for (int i=0; i<indexes.size(); i++) {
+            try {
+                indexFinal.add(ParserUtil.parseIndex(String.valueOf(indexes.get(i).getOneBased())));
+            } catch (IllegalValueException e) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+            }
+        }
+        */
+
+        return new DeleteCommand(indexes);
+
+        /*
         try {
             Index index = ParserUtil.parseIndex(args);
             return new DeleteCommand(index);
@@ -25,6 +68,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
+        */
     }
 
 }
